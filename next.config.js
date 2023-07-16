2466d083e77c03fbdc6ca67832416e1ac5fd9b18
Brandon Feldkamp <brandon.feldkamp@gmail.com>
2023-07-16 19:31:25 -0400

    set prefix:

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'docs'
+    distDir: 'docs',
+    assetPrefix: '/immaculate-gridiron'
 }
 
 module.exports = nextConfig
