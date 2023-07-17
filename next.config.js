bde1c0a76bd450d2e193b467dd1b28bb88748db9
Brandon Feldkamp <brandon.feldkamp@gmail.com>
2023-07-16 20:07:54 -0400

    remove prop

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
     output: 'export',
     distDir: 'docs',
-    assetPrefix: isProd ? 'https://bfeldkamp3.github.io/immaculate-gridiron/' : ''
+    assetPrefix: 'https://bfeldkamp3.github.io/immaculate-gridiron/'
 }
 
 module.exports = nextConfig
