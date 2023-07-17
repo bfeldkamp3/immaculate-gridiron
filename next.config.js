f7437c932eb1c5d5e062a62ce0c10e7a0f56ba64
Brandon Feldkamp <brandon.feldkamp@gmail.com>
2023-07-16 20:32:00 -0400

    add back in assetprefix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     output: 'export',
     distDir: 'docs',
-    // assetPrefix: isProd ? 'https://bfeldkamp3.github.io/immaculate-gridiron/' : '',
+    assetPrefix: isProd ? 'https://bfeldkamp3.github.io/immaculate-gridiron/' : '',
 }
 
 module.exports = nextConfig
